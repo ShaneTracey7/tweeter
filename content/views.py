cc8b31b01d5ac81ab315721b79358358baa0fb2f
Shane Tracey <71601674+ShaneTracey7@users.noreply.github.com>
2024-03-14 22:04:04 -0400

    added a little bit of home page

--- content/views.py
+++ content/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 def content(request):
-    template = loader.get_template('toolbar.html')
+    template = loader.get_template('homePage.html')
     return HttpResponse(template.render())
 
 
