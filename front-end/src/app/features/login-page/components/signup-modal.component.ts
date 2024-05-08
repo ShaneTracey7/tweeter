002fec9ac098e14d8c5898783d8aa13de583daec
Shane Tracey <71601674+ShaneTracey7@users.noreply.github.com>
2024-05-08 17:09:21 -0400

    worked on signup form validation css

--- front-end/src/app/features/login-page/components/signup-modal.component.ts
+++ front-end/src/app/features/login-page/components/signup-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter} from '@angular/core';
 import { getImgUrl } from '../../../core/data';
-import { FormControl, FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, ValidationErrors} from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, ValidationErrors, NgForm} from '@angular/forms';
 
 
 @Component({
@@ -19,17 +19,19 @@ export class SignupModalComponent {
 
 p_value = "password";
 
+submit_flag: number  = 0; // 0: not pressed, 1: pressed but not submitted, 2: pressed and submitted
+
 constructor(private formBuilder: FormBuilder) {}
 
 signupForm = this.formBuilder.group({
   name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(32), Validators.pattern('[a-zA-Z ]*')]],
   email: ['', [Validators.required,Validators.email]],
-  month: [0, Validators.required],
-  day: [0, Validators.required],
-  year: [0, [Validators.required, this.ageValidator]],
+  month: ['', Validators.required],
+  day: ['', Validators.required],
+  year: ['', [Validators.required, /*this.ageValidator*/]],
   username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(25)]],
   password1: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(25)]],
-  password2: ['', {validators: [Validators.required], updateOn: 'submit'}], 
+  password2: ['', [Validators.required],], 
   //make sure both passwords match (the submit works but idk if i want to go that route)
   //i have to find a way to not run only the required validation until it's submitted, the min/max length is find to go on default (onchangre)
   
@@ -47,7 +49,68 @@ ageValidator(): ValidatorFn {
         return +value > 2008 ? {age: true}: null;
     }
 }
-  
+
+isValidInput(fc: FormControl<any | null>)
+{
+  if (fc.valid)
+    {
+      return {
+        border: '1px solid lightgray',
+      };
+    }
+  else // fc is invalid
+  {
+    if(fc.pristine)
+      {
+        if(fc.touched)
+          {
+            return {
+              border: '2px solid red',
+            };
+          }
+        else //fc is untouched
+          {
+            if(this.submit_flag == 1)
+              {
+                return {
+                  border: '2px solid red',
+                };
+              }
+            else
+              {
+                return {
+                  border: '1px solid lightgray',
+                };
+              }
+          }
+      }
+    else //fc is dirty
+      {
+        return {
+          border: '2px solid red',
+        };
+      }
+  }
+}
+/*
+.ng-pristine.ng-untouched.ng-invalid {
+  border: 1px solid lightgray;
+}
+.ng-valid{
+  border: 1px solid lightgray;
+}
+
+.ng-invalid.ng-touched.ng-pristine{
+  border: 2px solid red;
+}
+.ng-invalid.ng-touched.ng-dirty{
+  border: 2px solid red;
+}
+
+
+
+*/
+/*
 isValidInput(check: boolean)
 {
   if (check)
@@ -63,6 +126,7 @@ isValidInput(check: boolean)
       };
     }
 }
+*/
 /*
 oldEnough(){
   
@@ -109,7 +173,20 @@ yearPlaceholderArray = new Array(100);
 */
 
 onSubmit(){
-  console.log("form submitted");
+
+  if(this.signupForm.valid)
+    {
+      console.log("form submitted");
+      this.submit_flag = 2;
+      this.signupForm.reset();
+    }
+  else
+  {
+    console.log("not submitted");
+    this.submit_flag = 1;
+  }
+
+  
 }
 
 
