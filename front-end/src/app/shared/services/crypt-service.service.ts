713db69615073897b596a63a711731247205818f
Shane Tracey <71601674+ShaneTracey7@users.noreply.github.com>
2024-05-31 16:27:09 -0400

    adding password encryption

---
+++ front-end/src/app/shared/services/crypt-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable} from "@angular/core";
+//import { CryptoJS } from './crypto-library.js';
+
+//JS FIX IV ENCRYPTION CBC
+
+
+@Injectable()
+export class CryptService {
+
+password = 'I love Medium'; // gonna be password
+key ='AAAAAAAAAAAAAAAA'//key used in Python
+
+encryptPassword(password: string)
+{
+    let key = CryptoJS.enc.Utf8.parse(this.key);
+    let iv = CryptoJS.enc.Utf8.parse('BBBBBBBBBBBBBBBB')
+    let encrypted = CryptoJS.AES.encrypt(password, key, { iv: iv, mode: CryptoJS.mode.CBC});
+    console.log('encrypted', encrypted.toString());
+    alert(encrypted)
+}
+
+}
