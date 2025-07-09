f057bf2f0d9b7cab0c1233ec679b210ae4b3fdc3
Shane Tracey <71601674+ShaneTracey7@users.noreply.github.com>
2025-07-09 13:13:43 -0400

    fix for render 7

--- back-end/tweeter/settings.py
+++ back-end/tweeter/settings.py
@@ -85,7 +85,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_HEADERS=True
 
 CORS_ALLOWED_ORIGINS = [
-"http://localhost:4200","https://thetweeters.netlify.app/",
+"http://localhost:4200","https://thetweeters.netlify.app",
 ]
 
 #
